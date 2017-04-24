b03372b7035bdc0120b3822b61dee944439432a8
J.P. Hamilton <instantjp@gmail.com>
2017-04-24 06:43:53 -0500

    initial sound

---
+++ src/sounds.ts
@@ -0,0 +1,12 @@
+import { Howl } from 'howler';
+
+
+const rumble = new Howl({
+    src: ['./assets/8bitrumble.wav'],
+    volume: 1
+});
+
+
+export { rumble };
+
+
--- src/states/attract.ts
+++ src/states/attract.ts
@@ -10,22 +10,66 @@ import { StarField } from '../starfield';
 import { Text } from '../text';
 import { Vector2 } from '../vector2';
 import { randomf } from '../util';
+import { rumble } from '../sounds';
 
-class Timer {
-    
-    private time: number = 0;
-    private trigger: number;
-    private cb: () => void;
+interface TimerOptions {
+    seconds: number;
+    delay?: number;
+    repeat?: boolean;
+}
 
-    constructor(value: number) {
-        this.trigger = value;
+class Timers {
+    private timers: Timer[] = [];
+
+    add(options: TimerOptions, cb: () => void) {
+        const timer = new Timer(options);
+        timer.on(cb);
+        this.timers.push(timer);
     }
 
     update(dt: number) {
+        this.timers.forEach(timer => timer.update(dt));
+    }
+}
+
+class Timer {
+    
+    private repeat: boolean;
+    private time: number = 0;
+    private delay: number;
+    private delayTime: number = 0;
+    private seconds: number;
+    private triggered: boolean;
+    private cb: () => void;
+
+    constructor(options: TimerOptions) {
+        this.seconds = options.seconds;
+        this.repeat = options.repeat !== undefined ? options.repeat : true;
+        this.delay = options.delay !== undefined ? options.delay : 0;
+    }
+
+    update(dt: number) {
+        if (this.triggered) {
+            return;
+        }
+
+        if (this.delay && this.delayTime < this.delay) {
+            this.delayTime += dt;
+            if (this.delayTime > this.delay) {
+                this.cb();
+            }
+            return;
+        }
+
         this.time += dt;
-        if (this.time >= this.trigger) {
+
+        if (this.time >= this.seconds) {
             this.time = 0;
             this.cb();
+
+            if (!this.repeat) {
+                this.triggered = true;
+            }
         } 
     }
 
@@ -43,8 +87,7 @@ export class AttractState implements IUpdateRender {
     private graphics: PIXI.Graphics;
     private filter: PIXI.Filter;
     private pushStart: Text;
-    private pushStartTimer: Timer;
-    private starTimer: Timer;
+    private timers: Timers;
     private camera2d: Camera;
     private time: number = 0;
 
@@ -93,14 +136,18 @@ export class AttractState implements IUpdateRender {
         }
 
         // timers
-        this.pushStartTimer = new Timer(.5);
-        this.pushStartTimer.on(() => {
+        this.timers = new Timers();
+
+        this.timers.add({ seconds: .5}, () => {
             this.pushStart.visible = !this.pushStart.visible;
         });
 
-        this.starTimer = new Timer(5);
-        this.starTimer.on(() => {
+        this.timers.add({ seconds: 5}, () => {
             changeStarVector();
+        });
+
+        this.timers.add({delay: 2, seconds: 10}, () => {
+            rumble.play();
         });
 
         // stars!
@@ -112,8 +159,7 @@ export class AttractState implements IUpdateRender {
     }
     
     update(dt: number) {
-        this.pushStartTimer.update(dt);
-        this.starTimer.update(dt);
+        this.timers.update(dt);
         
         this.time++;
         this.filter.uniforms.time = this.time * 0.5;
